744aabf51369b75ccad495a9444072bd77dc420f
koushin07 <43770631+koushin07@users.noreply.github.com>
2023-03-27 14:22:39 +0800

    version 1 complete

--- src/test/java/com/socmed/socmed/modules/post/PostServiceTest.java
+++ src/test/java/com/socmed/socmed/modules/post/PostServiceTest.java
@@ -10,21 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class PostServiceTest {
 
 
-    @Test
-    void testingnested() {
-        PostFeedsResponse.comments comment1 = PostFeedsResponse.comments.builder()
-                .content("aw").build();
-        PostFeedsResponse.comments comment2 = PostFeedsResponse.comments.builder()
-                .content("aw").build();
-        Collection<PostFeedsResponse.comments> list = new ArrayList<>();
-        list.add(comment1);
-        list.add(comment2);
-        PostFeedsResponse response = PostFeedsResponse.builder()
-                .PostContent("ye")
-                .comments(list.stream().toList()).build();
-        System.out.println(response);
-assertTrue(true);
-
-    }
+//    @Test
+//    void testingnested() {
+//        PostFeedsResponse.comments comment1 = PostFeedsResponse.comments.builder()
+//                .content("aw").build();
+//        PostFeedsResponse.comments comment2 = PostFeedsResponse.comments.builder()
+//                .content("aw").build();
+//        Collection<PostFeedsResponse.comments> list = new ArrayList<>();
+//        list.add(comment1);
+//        list.add(comment2);
+//        PostFeedsResponse response = PostFeedsResponse.builder()
+//                .PostContent("ye")
+//                .comments(list.stream().toList()).build();
+//        System.out.println(response);
+//assertTrue(true);
+//
+//    }
 
 }
